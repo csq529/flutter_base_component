9876ee1ef44e49c0a272f61b86f4020b58c53a62
csq529 <946081940@qq.com>
2026-03-03 17:20:18 +0800

    基本路由

---
+++ lib/27.路由-基本路由.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainPage());
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('列表页')),
+        body: ListView.builder(
+          itemCount: 100,
+          padding: EdgeInsets.all(10),
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage()));
+              },
+              child: Container(
+                padding: EdgeInsets.all(10),
+                height: 50,
+                alignment: Alignment.center,
+                color: Colors.blue,
+                margin: EdgeInsets.only(top: 10),
+                child: Text(
+                  '列表${index + 1}',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: TextButton(onPressed: () {
+          Navigator.pop(context);
+        }, child: Text('返回上一页'))
+      )
+    );
+  }
+}
